dd99ae637c3e0ea4b69234cd020e9b14f8626479
Kayal Rajkumar <kayal0306@gmail.com>
2021-04-22 15:29:22 -0700

    completed assignment 2

--- assignment.R
+++ assignment.R
@@ -11,26 +11,26 @@ rm(list = ls())
 
 # Load the `stringr` package (install it if you haven't used it before)
 # It has a variety of functions that make working with string variables easier
-
+library(stringr)
 
 # Create a variable `my_age` that is equal to your age
-
+my_age <- 20
 
 # Create a variable `my_name` that is equal to your first name
-
+my_name <- "Kayal"
 
 # Using a multiplication expression, create a variable `minutes_in_a_day` that
 # is equal to the number of minutes in a day
-
+minutes_in_a_day <- 24 * 60
 
 # Using a multiplication expression, create a variable `hours_in_a_year` that
 # is equal to the number of hours in a year
-
+hours_in_a_year <- 24 * 365
 
 # Create a variable `more_minutes_than_hours` that is a boolean (TRUE/FALSE)
 # It should be TRUE if there are more minutes/day than hours/year
 # Otherwise, it should be FALSE
-
+more_minutes_than_hours <- minutes_in_a_day > hours_in_a_year
 
 ### Working with Functions ####################################################
 
@@ -43,59 +43,68 @@ rm(list = ls())
 # spaces between words, and you should have a space after a comma)
 # Hint: Google search for "R paste paste0 difference"
 #       Try reading a few pages to understand how to use each
-
+make_introduction <- function(name, age) {
+  paste0("Hello, my name is", sep = "_", name, ", and I'm", sep = "_", age,
+         sep = "_", "years old.")
+}
 
 # Create a variable `my_intro` by passing your variables `my_name` and `my_age`
 # into your `make_introduction()` function
-
+my_intro <- make_introduction(my_name, my_age)
 
 # Create a variable `casual_intro` by substituting "Hello, my name is",
 # with "Hey, I'm" in your `my_intro` variable.
 # Hint: Take a look at the sub() function
-
+casual_intro <- sub("Hello, my name is", "Hey, I'm", my_intro)
 
 # Create a variable `loud_intro`, which is `my_intro` in all upper-case letters
 # You should do this by using a function to convert your `my_intro` variable
 # into all capital letters
-
+loud_intro <- toupper(my_intro)
 
 # Create a variable `quiet_intro`, which is `my_intro` in all lower-case letters
 # You should do this by using a function to convert your `my_intro` variable
 # into all lower-case letters
-
+quiet_intro <-tolower(my_intro)
 
 # Create a new variable `capitalized_intro`, which is your `my_intro` variable
 # but with each word capitalized
 # Hint: Google search the stringr function `str_to_title`
-
+capitalized_intro <- str_to_title(my_intro)
 
 # Using the `str_count` function from stringr, create a variable `occurrences`
 # that stores the # of times the letter "e" appears in `my_intro`
-
+occurrences <- str_count(my_intro, "e")
 
 # Write a function `double()` that takes in a value and
 # returns that value multiplied by 2
-
+double <- function(value) {
+  value * 2
+}
 
 # Using your `double()` function, create a variable `minutes_in_two_days`,
 # which is the number of minutes in two days
-
+minutes_in_two_days <- double(minutes_in_a_day)
 
 # Write a function `cube()` that takes in a value and returns that value cubed
-
+cube <- function(value) {
+  value * value * value
+}
 
 # Create a variable `twenty_seven` by passing 3 to your `cube()` function
-
+twenty_seven <- cube(3)
 
 # Create a function `inches_to_cm` that converts from inches to centimeters
-
+inches_to_cm <- function(inches) {
+  inches * 2.54
+}
 
 # Create a variable `inches_tall` that is your (numeric) height in inches
-
+inches_tall <- 64
 
 # Using your `inches_to_cm` function and your `inches_tall` variable,
 # create a variable `cm_tall` that is your height in centimeters
-
+cm_tall <- inches_to_cm(inches_tall)
 
 # Write a function `has_more_zs` to determine which of two strings contains
 # more instances of the letter "z". It should take as parameters two string
@@ -105,73 +114,88 @@ rm(list = ls())
 # If the phrases contain the same number of "z"s, it should return:
 # "The strings have the same number of Zs."
 # The function must work for both capital and lowercase "z"s.
-
+has_more_zs <- function(str1, str2) {
+  instances_of_z_str1 <- str_count(str1, "z") + str_count(str1, "Z")
+  instances_of_z_str2 <- str_count(str2, "z") + str_count(str2, "Z")
+  if (instances_of_z_str1 == 0 & instances_of_z_str2 == 0) {
+    sentence <- paste("Neither string contains the letter z.")
+  } else if (instances_of_z_str1 > instances_of_z_str2) {
+    sentence <- paste("String 1 has more instances of zs than string 2.")
+  } else if (instances_of_z_str2 > instances_of_z_str1) {
+    sentence <- paste("String 2 has more instances of zs than string 1.")
+  } else {
+    sentence <- paste("The strings have the same number of Zs.")
+  }
+  sentence
+}
 
 # Create a variable `more_zs` by passing two strings of your choice to your
 # `has_more_zs` function
-
+more_zs <- has_more_zs("I am tired.", "I am going to the park now.")
 
 # Write a function `remove_digits` that will remove all digits
 # (i.e., 0 through 9) from all elements in a *vector of strings*.
-
+remove_digits <- function(strings) {
+  gsub("[0-9]", "", strings)
+}
 
 # Demonstrate that your approach is successful by passing a vector of courses
 # to your function. For example, remove_digits(c("INFO 201", "CSE 142"))
-
+practice <- remove_digits(c("INFO 201", "PSYCH 318", "PSYCH 448"))
 
 ### Vectors ###################################################################
 
 # Create a vector `movies` that contains the names of six movies you like
-
+movies <- c("Up", "Tangeled", "Moana", "Cars", "Soul", "Mulan")
 
 # Create a vector `top_three` that only contains the first three movies of
 # your `movies` list (e.g., index 1 through index 3)
 # You should do this by subsetting the vector, do not just retype the movies
-
+top_three <- movies[1:3]
 
 # Using your vector and the `paste()` method, create a vector `excited` that
 # adds the phrase " is a great movie!" to the end of each element in `movies`
-
+excited <- paste(movies, "is a great movie!")
 
 # Create a vector `without_four` that omits the fourth element from `movies`
 # You should do this by using a _negative index_
-
+without_four <- movies[-4]
 
 # Create a vector `multiples_of_4` that is every number divisible by 4
 # between 4 and 400 (**2 points**)
-
+multiples_of_4 <- seq(4, 400, 4)
 
 # Create a vector `multiples_of_8` by filtering your `multiples_of_4` variable
 # down to only elements that are divisible by 8.
 # Hints:
 # - See chapter 7.4 in the book for vector filtering
 # - Google search "modulo operator in R"
-
+multiples_of_8 <- multiples_of_4[multiples_of_4 %% 8 == 0]
 
 # Create a vector `numbers` that is the numbers 700 through 999
-
+numbers <- seq(700, 999)
 
 # Using the built in `length()` function, create a variable `numbers_len`
 # that is equal to the length of the vector `numbers`
-
+numbers_len <- length(numbers)
 
 # Using the `mean()` function, create a variable `numbers_mean` that is
 # equal to the mean of the vector `numbers`
-
+numbers_mean <- mean(numbers)
 
 # Using the `median()` function, create a variable `numbers_median`
 # that is the median of the vector `numbers`
-
+numbers_median <- median(numbers)
 
 # Create a vector `lower_numbers` that the values in the `numbers` vector
 # that are lower than `numbers_mean`
 # Hint: Use vector filtering)
-
+lower_numbers <- numbers[numbers < numbers_mean]
 
 # Create a vector `higher_numbers` that the values in the `numbers` vector
 # that are higher than `numbers_mean`
 # Hint: Again, use vector filtering
-
+higher_numbers <- numbers[numbers > numbers_mean]
 
 ### Lists #####################################################################
 
@@ -181,67 +205,74 @@ rm(list = ls())
 # - `length`: in which you'll store the length of the vector
 # - `mean`: in which you'll store the mean of the vector
 # - `median`: in which you'll store the median of the vector
-
+summary_info <- list(length = numbers_len, mean = numbers_mean, median = numbers_median)
 
 # Now, write a function called `summarize_vector` that takes in a vector of
 # numbers, and returns a list of summary information about that vector
 # (including the mean, median, and length)
-
+summarize_vector <- function(numbers_vector) {
+  vector_mean <- mean(numbers_vector)
+  vector_median <- median(numbers_vector)
+  vector_length <- length(numbers_vector)
+  list(mean = vector_mean, median = vector_median, length = vector_length)
+}
 
 # Create a list `summary_1_to_100` by passing a vector of the values one
 # through one hundred to your `summarize_vector` function
-
+values_one_through_hundred <- seq(1, 100)
+summary_1_to_100 <- summarize_vector(values_one_through_hundred)
 
 ### Data Frames ###############################################################
 
 # Create a vector `students` holding 1,000 values representing students
 # They should have the values "Student 1", "Student 2",..., "Student 1000"
-
+students <- paste("Students", 1:1000)
 
 # Create a vector `math_grades` that holds 1000 random values in it that
 # represent grades in a math course
 # These values should be normally distributed with a mean of 88 and a
 # standard deviation of 10
 # Hint: Lookup `rnorm()`
-
+math_grades <- rnorm(1000, 88, 10)
 
 # In the `math_grades` vector, replace any values that are above 100 with
 # the number 100
 # Hint: Vector filtering
-
+math_grades[math_grades > 100] <- 100
 
 # Create a vector `spanish_grades` that holds 1000 random values in it that
 # represent grades in a spanish course
 # These values should be normally distributed with a mean of 85 and a
 # standard deviation of 12
 # Hint: Lookup `rnorm()`
-
+spanish_grades <- rnorm(1000, 85, 12)
 
 # In the `spanish_grades` vector, replace any values that are above 100 with
 # the number 100
 # Hint: More vector filtering
-
+spanish_grades[spanish_grades > 100] <- 100
 
 # Create a data frame variable named `grades` by combining
 # the vectors `students`, `math_grades`, and `spanish_grades`
-
+grades <- data.frame(students, math_grades, spanish_grades)
 
 # Create a variable `num_students` that counts the
 # number of rows in your dataframe `grades`
-
+num_students <- nrow(grades)
 
 # Create a variable `num_courses` that counts the number of courses stored
 # in the columns of your `grades` data frame
-
+num_courses <- ncol(grades)
 
 # Add a new column `grade_diff` to your data frame, which is equal to
 # `grades$math_grades` minus `grades$spanish_grades`
-
+grades$grade_diff <- grades$math_grades - grades$spanish_grades
 
 # Add another column `better_at_math` as a boolean (TRUE/FALSE) variable that
 # indicates that a student got a better grade in math
-
+grades$better_at_math <- grades$grade_diff > 0
 
 # Create a variable `num_better_at_math` that is the number
 # (i.e., one numeric value) of students better at math
+num_better_at_math <- nrow(grades[grades$better_at_math == TRUE, ])
 
